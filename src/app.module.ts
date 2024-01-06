ff4e6451a8eb668ce9e22e6a9802baeee8f90057
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-01-06 15:10:13 +0900

    :wrench: Chore: 환경변수 .env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '@src/config/config.module';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { config } from 'dotenv';
+import * as path from 'path';
+
+@Injectable()
+export class ConfigService {
+  public readonly discordToken: string;
+  public readonly discordClientId: string;
+
+  constructor() {
+    config({
+      path: path.resolve(
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+      ),
+    });
+
+    this.discordToken = process.env.DISCORD_API_TOKEN;
+    this.discordClientId = process.env.DISCORD_CLIENT_ID;
+
+    Logger.log(this.discordToken);
+    Logger.log(this.discordClientId);
+  }
+}
